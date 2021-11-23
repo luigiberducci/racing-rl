b2312b4855f9ab33c0b908f926b5176245a8bff9
Luigi Berducci <luigi.berd@gmail.com>
2021-11-23 15:28:35 +0100

    adapt purepursuit from f110gym to the wrapped env, update example to use it

--- example.py
+++ example.py
@@ -1,15 +1,44 @@
 import time
 
 from gym.wrappers import TimeLimit
-
-from racing_rl.envs.wrappers import FixResetWrapper, LapLimit, ElapsedTimeLimit
-from racing_rl.training.env_utils import make_base_env
-
-env = make_base_env('SingleAgentMelbourne-v0')
-env = FixResetWrapper(env, mode='random')
-env = ElapsedTimeLimit(env, max_episode_duration=10.0)
-
 import matplotlib.pyplot as plt
+
+from racing_rl.baseline.PurePursuitPlanner import PurePursuitPlanner
+from racing_rl.envs.wrappers import FixResetWrapper, LapLimit, ElapsedTimeLimit, LidarOccupancyObservation
+from racing_rl.rewards.progress_based import ProgressReward
+from racing_rl.training.env_utils import make_base_env
+import racing_rl
+import gym
+
+env = gym.make('SingleAgentMelbourne-v0')
+env = ProgressReward(env, env.track)
+env = LidarOccupancyObservation(env, max_range=10.0, resolution=0.25)
+env = FixResetWrapper(env, mode='random')
+env = ElapsedTimeLimit(env, max_episode_duration=20.0)
+
+
+def render_callback(env_renderer):
+    # custom extra drawing function
+    e = env_renderer
+
+    # update camera to follow car
+    x = e.cars[0].vertices[::2]
+    y = e.cars[0].vertices[1::2]
+    top, bottom, left, right = max(y), min(y), min(x), max(x)
+    e.score_label.x = left
+    e.score_label.y = top - 700
+    e.left = left - 800
+    e.right = right + 800
+    e.top = top + 800
+    e.bottom = bottom - 800
+
+    planner.render_waypoints(env_renderer)
+
+env.add_render_callback(render_callback)
+
+planner = PurePursuitPlanner(env.track, wb=0.17145 + 0.15875, fixed_speed=5.0)
+
+
 
 for i in range(5):
     t0 = time.time()
@@ -18,11 +47,12 @@ for i in range(5):
     t = 0
     while not done:
         t += 1
-        obs, reward, done, info = env.step([0.0, 0.0])
-        #print(reward)
-        if info['collision']:
-            print("COLLISION")
+        speed, steer = planner.plan(obs['pose'][0], obs['pose'][1], obs['pose'][2], lookahead_distance=1.5, vgain=1.0)
+        obs, reward, done, info = env.step({'steering': steer, 'velocity': speed})
+        # print(reward)
         env.render()
-        plt.imshow(obs)
-        plt.show()
+        if t % 10 == 0:
+            plt.clf()
+            plt.imshow(obs['lidar_occupancy'])
+            plt.pause(0.01)
     print(f"DONE, sim time: {info['lap_time']}, real time: {time.time() - t0}")
