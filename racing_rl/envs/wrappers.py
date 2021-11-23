bugfix lidar occupancy computation, set car min velocity, other minor changes

racing_rl/envs/wrappers.py
@@ -28,7 +28,7 @@ class LidarOccupancyObservation(gym.ObservationWrapper):
         xi, yi = np.floor(xx / res), np.floor(yy / res)
         for px, py in zip(xi, yi):
             row = min(max(n_bins // 2 + py, 0), n_bins - 1)
-            col = min(max(n_bins // 2 + py, 0), n_bins - 1)
+            col = min(max(n_bins // 2 + px, 0), n_bins - 1)
             if row < n_bins - 1 and col < n_bins - 1:
                 # in this way, then >max_range we dont fill the occupancy map in order to let a visible gap
                 occupancy_map[int(row), int(col)] = 255