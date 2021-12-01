603ca98b5fbe9538d92512273fc832c5a7665500
Luigi Berducci <luigi.berd@gmail.com>
2021-12-01 13:11:03 +0100

    create experiment script for collision_penalty

---
+++ scripts/run_experiment_collision_penalty.py
@@ -0,0 +1,39 @@
+# experiment: experiments for various collision_penalty in reward shaping
+# date: 1 Dec 2021
+# aim:  evaluate the impact of the collision penalty in the reward shaping
+#
+# exp. setup: evaluate PPO under various reward formulations (0.0, 1.0, 5.0, 10.0, 15.0, 20.0)
+#             mdp with observation space (2d-occupancymap, velocity), reward 'min-action', 2 controls (steer,speed)
+#             train PPO with MultiInput policy (CNN feature extractor for images -> concat -> MLP policy)
+#             n_steps: 50K, n_seeds: 5
+#
+# how to run (6 separate instances of 5 seeds each):
+#     $>python script/run_experiment_collision_penalty.py --n_seeds 5 --collision_penalty 0.0
+#     ...
+#     $>python script/run_experiment_collision_penalty.py --n_seeds 5 --collision_penalty 20.0
+
+import argparse
+from argparse import Namespace
+
+import numpy as np
+
+from train import train
+
+params = {
+    'track': "melbourne",
+    'reward': "min_action",
+    'algo': "ppo",
+    'only_steering': False,
+    'n_steps': 50000,
+}
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_seeds", type=int, required=True)
+parser.add_argument("--collision_penalty", type=float, required=True)
+args = parser.parse_args()
+
+for i in range(args.n_seeds):
+    current_params = params
+    current_params['seed'] = np.random.randint(0, 1000000)
+    current_params['collision_penalty'] = args.collision_penalty
+    train(Namespace(**params))
